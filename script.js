26378f5e6988c70df99a945c9b11434839686252
Flávia lemes <105284601+Flavialemes@users.noreply.github.com>
2024-09-05 07:52:44 -0300

    ok

--- script.js
+++ script.js
@@ -7,43 +7,44 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Como você acredita que podemos combater o racismo na sociedade?",
+        enunciado: "Qual é uma vantagem de ter um ateliê de costura especializado em peças sob medida?",
         alternativas: [
             {
-                texto: "Participando de campanhas de conscientização e educação sobre igualdade racial.",
-                afirmacao: "afirmacao1"
+                texto: "Personalização completa das peças",
+                afirmacao: "Um ateliê de costura sob medida permite que os clientes obtenham roupas ajustadas perfeitamente ao seu corpo e estilo, garantindo um ajuste ideal."
             },
             {
-                texto: "Denunciando atos de discriminação e apoiando políticas públicas inclusivas.",
-                afirmacao: "afirmacao2"
+                texto: "Redução de custos em comparação com marcas de luxo.",
+                afirmacao: "Embora a personalização possa oferecer um ajuste superior, os custos de roupas sob medida podem ser mais altos devido ao trabalho artesanal e aos materiais utilizados."
             }
            
         ]
     },
     {
-        enunciado: "Como você pode ajudar a combater a disseminação de fake news, especialmente as que perpetuam o racismo?",
+        enunciado: "Como a sustentabilidade pode ser integrada em um ateliê de costura?",
         alternativas: [
             {
-                texto: "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas sensíveis como racismo.",
-                afirmacao: "afirmacao3"
+                texto: "Utilização de tecidos reciclados e orgânicos",
+                afirmacao: "Incorporar tecidos reciclados e orgânicos ajuda a reduzir o impacto ambiental da produção de roupas, promovendo práticas mais sustentáveis."
+
             },
             {
-                texto: "Educando amigos e familiares sobre os perigos das fake news e incentivando-os a não compartilhar conteúdos falsos que perpetuam o racismo.",
-                afirmacao: "afirmacao4"
+                texto: "Oferecer serviços de reforma e conserto de roupas.",
+                afirmacao: " Serviços de reforma e conserto ajudam a prolongar a vida útil das roupas, diminuindo a necessidade de novos produtos e reduzindo o desperdício."
             }
            
         ]
     },
     {
-        enunciado: "Como podemos incentivar o uso de energia solar e outras formas de energia limpa, especialmente em comunidades carentes?",
+        enunciado: "Qual é um benefício da digitalização no processo de design e produção de roupas em um ateliê?",
         alternativas: [
             {
-                texto: "Defendendo políticas públicas que subsidiem a energia solar para famílias de baixa renda.",
-                afirmacao:"afirmacao5"
+                texto: "Aumento da eficiência no processo de design",
+                afirmacao:"O uso de softwares de design e modelagem digital permite uma criação mais rápida e precisa de padrões, economizando tempo e recursos."
             },
             {
-                texto: "Promovendo a instalação de painéis solares em escolas e centros comunitários de comunidades marginalizadas.",
-                afirmacao:"afirmacao6"
+                texto: "Possibilidade de produção em massa com menos personalização",
+                afirmacao:"Embora a digitalização facilite a produção em larga escala, ela pode limitar a personalização, a menos que sejam implementadas soluções específicas para customização."
             }
         ]
     }
